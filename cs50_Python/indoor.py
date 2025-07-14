f47e8d4072facd43d944f0a51aa5f880711fb6de
Kemo-Kat <154595367+Kemo-Kat@users.noreply.github.com>
2025-07-14 10:35:56 +0200

    Add files via upload

---
+++ cs50_Python/indoor.py
@@ -0,0 +1,9 @@
+# implement a program in Python that prompts
+# the user for input and then outputs that
+# same input in lowercase. Punctuation and
+# whitespace should be outputted unchanged
+
+indoor_voice = input("Use your indoor voice: ")
+indoor_voice = indoor_voice.lower()
+
+print(f"This is your indoor voice: {indoor_voice}")
---
+++ cs50_Python/playback.py
@@ -0,0 +1,8 @@
+# implement a program in Python that prompts the
+# user for input and then outputs that same input,
+# replacing each space with " ... "
+
+
+playback_speed = input("--> ")
+playback_speed = playback_speed.replace(" ", "...")
+print(playback_speed)
